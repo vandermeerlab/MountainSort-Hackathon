564e22de9933a4dddfacb5d7c396d40bc3e192f8
ecarmichael <jericcarmichael@Js-MacBook-Pro-2.local>
2018-05-03 21:21:32 -0400

    write mda is kinda working?

--- ncs2mda.m
+++ ncs2mda.m
@@ -5,20 +5,27 @@
     addpath(genpath('/Users/jericcarmichael/Documents/GitHub/MountainSort-Hackathon-Matlab'))
 %     addpath(genpath('D:\Users\mvdmlab\My_Documents\GitHub\MountainSort-Hackathon-Matlab'))
 % move to the data
-cd('/Users/jericcarmichael/Documents/R050-2014-03-28_32ktest')
 
 %% add mountainlab
 cd('/Users/jericcarmichael/Documents/GitHub/mountainsort/matlab')
 run mlsetup.m
+cd('/Users/jericcarmichael/Documents/R050-2014-03-28_32ktest')
 
 %% get the ncs files
 cfg.fc={'test1.ncs', 'test2.ncs', 'test3.ncs', 'test4.ncs'};
 test = LoadCSC(cfg);
 
-data_out = test.data;
+data_out = test.data*1000000;
 
-
+subplot(2,1,1)
+plot(data_out(1,:)')
 
 % prepare your MxN array called X %
 
-writemda16i(data_out,'raw2.mda');
+writemda16i(data_out,'raw.mda');
+
+%%
+clear all
+X=readmda('raw1.mda');
+subplot(2,1,2)
+plot(X(1,:))
